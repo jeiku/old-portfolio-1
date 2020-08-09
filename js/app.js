c12aae1c00a5b1857717b3296978fe9bf6715a91
Jake <9012257+jeiku@users.noreply.github.com>
2020-08-08 22:29:05 -0400

    added buttons and images

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
+window.sr = ScrollReveal();
 const resume = document.getElementById("resume");
 const resumeLink = document.getElementById("resume-link");
 
 function confirmation(e) {
-    // if(confirm("Would you like to view the PDF?")) {
+	// if(confirm("Would you like to view the PDF?")) {
 
-    // }
-    if (!confirm('Would you like to view my resume as a PDF?')) {
-        e.preventDefault();
-    } else {
-        window.open("../sample.pdf");
-    }
+	// }
+	if (!confirm("Would you like to view my resume as a PDF?")) {
+		e.preventDefault();
+	} else {
+		window.open("../sample.pdf");
+	}
 }
+
+sr.reveal(".animate-up", {
+	origin: "bottom",
+	duration: 1000,
+	distance: "25rem",
+	delay: 400,
+});
 
 resume.addEventListener("click", confirmation);
 resumeLink.addEventListener("click", confirmation);
