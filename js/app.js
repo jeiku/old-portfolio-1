6e4dd9bf704ae7334fa92ec204ff3842bd3152e0
jake <9012257+jeiku@users.noreply.github.com>
2020-10-17 17:33:27 -0400

    updated links

--- js/app.js
+++ js/app.js
@@ -3,21 +3,18 @@ const resume = document.getElementById("resume");
 const resumeLink = document.getElementById("resume-link");
 
 function confirmation(e) {
-	// if(confirm("Would you like to view the PDF?")) {
-
-	// }
-	if (!confirm("Would you like to view my resume as a PDF?")) {
-		e.preventDefault();
-	} else {
-		window.open("../jacob-west-resume.pdf");
-	}
+  if (!confirm("Would you like to view my resume as a PDF?")) {
+    e.preventDefault();
+  } else {
+    window.open("../jacob-west-resume.pdf");
+  }
 }
 
 sr.reveal(".animate-up", {
-	origin: "bottom",
-	duration: 1000,
-	distance: "25rem",
-	delay: 400,
+  origin: "bottom",
+  duration: 500,
+  distance: "9rem",
+  delay: 120,
 });
 
 resume.addEventListener("click", confirmation);
